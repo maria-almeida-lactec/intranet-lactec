4fd2abd1d096c0241e3359dc9cee831e7b3177ad
Maria Eduarda Benites de Almeida <maria.almeida@lactec.com.br>
2026-01-13 11:10:50 -0300

    Exercício 17: Testes de configuração do backend

--- backend/tests/setup/test_portal_settings.py
+++ backend/tests/setup/test_portal_settings.py
@@ -2,16 +2,29 @@
 
 from plone import api
 
+import pytest
+
 
 class TestPortalSettings:
     """Test that Portal configuration is correctly done."""
 
-    def test_portal_title(self, portal):
-        """Test portal title."""
-        value = api.portal.get_registry_record("plone.site_title")
-        assert value == "Intranet da Lactec"
-
-    def test_navigation_depth(self, portal):
-        """Test navigation depth."""
-        value = api.portal.get_registry_record("plone.navigation_depth")
-        assert value == 3
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ("plone.site_title", "Intranet da Lactec"),
+            ("plone.email_from_name", "Intranet Lactec"),
+            ("plone.smtp_host", "localhost"),
+            ("plone.smtp_port", 25),
+            ("plone.available_timezones", ["America/Sao_Paulo"]),
+            ("plone.portal_timezone", "America/Sao_Paulo"),
+            ("plone.first_weekday", 6),
+            ("plone.navigation_depth", 3),
+            ("plone.twitter_username", ""),
+            ("plone.available_languages", ["pt-br"]),
+            ("plone.default_language", "pt-br"),
+        ],
+    )
+    def test_setting(self, portal, key: str, expected: str | int | list[str]):
+        """Test registry setting."""
+        value = api.portal.get_registry_record(key)
+        assert value == expected
