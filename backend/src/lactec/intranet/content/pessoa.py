7af5c47e4dea6f3bde83af4ec3e809f3b39aa619
Maria Eduarda Benites de Almeida <maria.almeida@lactec.com.br>
2026-01-14 11:53:03 -0300

    Adicionar o campo Cargo, utilizando um vocabulário controlado, ao tipo Pessoa

--- backend/src/lactec/intranet/content/pessoa.py
+++ backend/src/lactec/intranet/content/pessoa.py
@@ -1,12 +1,20 @@
+from lactec.intranet import _
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from zope import schema
 from zope.interface import implementer
 
 
 class IPessoa(model.Schema):
     """Definição de uma Pessoa."""
 
+    cargo = schema.Choice(
+        title=_("Cargo"),
+        vocabulary="lactec.intranet.vocabulary.cargos",
+        required=False,
+    )
+
 
 @implementer(IPessoa)
 class Pessoa(Container):
-    """Uma Pessoa dentro da Lactec."""
+    """Uma Pessoa no Lactec."""
